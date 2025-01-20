2eaf591bd286af7b17ff9d13659ae1acbeb79834
박기찬 <rl0425@naver.com>
2025-01-20 21:49:11 +0900

    feat: Add title components

---
+++ src/app/_components/_title/title.tsx
@@ -0,0 +1,8 @@
+const Title = () => {
+  const title = "PARK GI CHAN";
+  return (
+    <p className="pr-5 pt-3 text-right text-[28px] font-extrabold">{title}</p>
+  );
+};
+
+export default Title;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,14 @@ import SideProject from "./_components/_side-project/page";
 import Contact from "./_components/_contact/page";
 import Channel from "./_components/_channel/page";
 import Skills from "./_components/_skills/page";
+import Title from "./_components/_title/title";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <div className="mx-auto min-h-screen max-w-[430px] bg-white md:max-w-[710px]">
         <main className="h-full w-full">
-          <p className="pr-5 pt-3 text-right text-[28px] font-extrabold">
-            PARK GI CHAN
-          </p>
+          <Title />
           <Introduce />
           <WorkExperience />
           <Skills />
